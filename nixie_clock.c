f33aafc291e2bd483298d8e8b17d7f273b2f552e
Christian C. Gruber <christian.gruber@acib.at>
2011-06-03 12:22:38 +0200

    First version

---
+++ nixie_clock.c
@@ -0,0 +1,117 @@
+// ************************************************************************************
+// *           Userland driver for the 8 digits Nixie Clock/Counter from              *
+// *  http://www.kosbo.com/index.php?option=com_content&view=article&id=51&Itemid=60  *
+// *                                  Version 0.1                                     *
+// *                     Copyright by Christian C. Gruber 2011                        *
+// ************************************************************************************
+
+#include <stdio.h>
+#include <sys/time.h>
+#include <termios.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <termios.h>
+
+main( int argc, char *argv[] )
+{
+  // Three command line arguments are required
+  // Usage information
+  if ( argc != 4 )
+  {
+    fprintf(stderr,"Usage: %s [UTC led device] [nixie tubes device] [delay in us] (Example: %s /dev/led/gpio11 /dev/ttyu0 1000) \n", argv[0], argv[0] );
+  }
+  // Starting
+  else
+  {
+    int delay = atoi(argv[3]);
+    fprintf(stderr,"%s Version 0.1\nCopyright 2011 Christian C. Gruber <cgchilia.com>\nUTC LED device = %s\nNixie tubes device = %s \nDelay in us = %d\n", argv[0], argv[1], argv[2], delay );
+    FILE *output;
+    FILE* led;
+    FILE* port;
+    int offset;
+    char *state;
+    struct timeval tv;
+    struct timezone tz;
+    struct tm *tm;  
+    state = "init";
+    offset = 0;
+    port = fopen(argv[2], "wb+" );
+    for (;;)
+    {
+      gettimeofday(&tv,&tz);
+      tm=localtime(&tv.tv_sec);
+      // Tick
+      if ( tv.tv_usec <= delay )
+      {
+        // Output offset in ns between the 20th and 25th second 
+        if ( ( tm->tm_sec > 20 ) && ( tm->tm_sec < 25 ) )
+        {
+          if ( offset == 0 )
+          {
+          fprintf(port,"%08s\n", "        ");
+          }
+          else if ( offset > 99999999 )
+          {
+          fprintf(port,"%08s\n", "99999999");
+          }
+          else
+          {
+          fprintf(port,"%08d\n", offset);
+          }
+        }
+        // Output date between the 40th and 45th second
+        else if ( ( tm->tm_sec > 40 ) && ( tm->tm_sec < 45 ) )
+        {
+          fprintf(port,"%02d %02d %02d\n", tm->tm_mday, tm->tm_mon, tm->tm_year - 100 );
+        }
+        // Output time
+        else
+        {
+          fprintf(port, "%02d %02d %02d\n", tm->tm_hour, tm->tm_min, tm->tm_sec);
+        }
+        // Timing statistics go to stderr
+        fprintf(stderr,"The %02d:%02d:%02d tick is %04d us late, but within a %d us range (offset: %08d, %s).\n", tm->tm_hour, tm->tm_min, tm->tm_sec, tv.tv_usec, delay, offset, state);
+        // Light red led when ntpd is not running
+        if ( offset == 0 )
+        {
+          state="off";
+          led = fopen(argv[1], "w" );
+          fprintf(led,"1");
+          fclose(led);
+        }
+        // Wild blink red led during synchronisation
+        else if ( offset > delay * 1000 )
+        {
+          state="syncing";
+          led = fopen(argv[1], "w" );
+          fprintf(led,"f1");
+          fclose(led);
+        }
+        // Blink led synchronised to UTC time tick
+        else if ( offset <= delay * 1000 )
+        {
+          state="synced";
+          led = fopen(argv[1], "w" );
+          fprintf(led,"1");
+          fclose(led);
+          usleep(1000);
+          led = fopen(argv[1], "w" );
+          fprintf(led,"0");
+          fclose(led);
+        }  
+        // Get ntpd offset
+        output = popen(" printf '%08s' `ntpdc -c kern 2> /dev/null| sed 's/-//g' | awk ' /^pll offset:/ { print $3 * 1000000000 }'` ", "r");
+        fscanf( output, "%d", & offset );
+        pclose(output);
+        // This half a second sleep is important for cpu load descrase ...
+        usleep(500000);
+      }
+    }
+  fclose(port);
+  }
+  return 0;
+}
